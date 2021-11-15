392c99b788d3767b448224107d5d9bbe0c416c0e
Joseph Pallipadan <nobunaga@Josephs-MacBook-Pro.local>
2021-11-14 22:30:50 -0500

    Other Car spwans and moves

--- scen2/game.js
+++ scen2/game.js
@@ -20,6 +20,11 @@ const config = {
 const game = new Phaser.Game(config);
 const lanePositions = [354, 470, 600, 730];
 
+const timers = {
+    otherCar: 0,
+    malfunction: 0
+}
+
 function preload ()
 {
     this.load.image('road', 'road.png');
@@ -38,19 +43,26 @@ function create ()
     this.player.setScale(0.25);
     this.player.setRotation(-Math.PI / 2);
 
+    this.otherCar = this.add.image(lanePositions[0] + 60, 500, 'player');
+    this.otherCar.setScale(0.25);
+    this.otherCar.setTint(0xfffff);
+    this.otherCar.setRotation(-Math.PI / 2);
+
     this.cursors = this.input.keyboard.createCursorKeys();
 
     this.camera = this.cameras.main;
 
     this.logs = [];
 
-    this.activateButton = this.add.text(25, 10, 'Toggle HFDS', { fontSize: '20px', fill: '#0f0', fontFamily: 'Arial' });
+    this.activateButton = this.add.text(25, 10, 'Deactivate\nHFDS', { fontSize: '20px', fill: '#0f0', fontFamily: 'Arial' });
     this.activateButton.setInteractive()
     .on('pointerdown', () => handleHFDSClick.call(this) )
     .on('pointerover', () => this.activateButton.setStyle({ fill: '#ff0'}) )
     .on('pointerout', () => this.activateButton.setStyle({ fill: '#0f0' }) );
 
-    this.hfdsEnabled = false;
+    this.hfdsEnabled = true;
+
+    if (this.hfdsEnabled) activateHFDS.call(this);
 }
 
 function handleHFDSClick() {
@@ -60,11 +72,13 @@ function handleHFDSClick() {
 
 function activateHFDS() {
     this.hfdsEnabled = true;
+    this.activateButton.text = "Deactivate\nHFDS";
     addLog.call(this, "System Activated");
 }
 
 function deactivateHFDS() {
     this.hfdsEnabled = false;
+    this.activateButton.text = "Activate\nHFDS";
     addLog.call(this, "System Deactivated");
 }
 
@@ -72,20 +86,16 @@ function update(time, delta)
 {
     const cursors = this.cursors;
     const offset = 100 * delta / 1000;
+
     if (cursors.left.isDown)
     {
         this.player.x -= offset;
+        if (this.hfdsEnabled) deactivateHFDS.call(this);
     }
     else if (cursors.right.isDown)
     {
         this.player.x += offset;
-    }
-    if (cursors.up.isDown)
-    {
-        handleForwardMovement();
-        if (this.hfdsEnabled) {
-            deactivateHFDS.call(this);
-        }
+        if (this.hfdsEnabled) deactivateHFDS.call(this);
     }
 
     const handleForwardMovement = () => {
@@ -94,8 +104,25 @@ function update(time, delta)
         this.activateButton.y -= offset;
     };
 
+    if (cursors.up.isDown || this.hfdsEnabled)
+    {
+        handleForwardMovement();
+        if (cursors.up.isDown && this.hfdsEnabled) {
+            deactivateHFDS.call(this);
+        }
+    }
+
     this.camera.centerOnY(this.player.y - 200);
     fixRoads.call(this);
+
+    const otherCar = this.otherCar;
+    otherCar.y -= offset * 2;
+    Object.keys(timers).forEach((key) => timers[key] += delta / 1000);
+    if (timers.otherCar >= 5 && !this.camera.worldView.contains(otherCar.x, otherCar.y + 100)) {
+        otherCar.y = this.player.y + 500;
+        otherCar.x = lanePositions[Math.random() < 0.5 ? 0 : 1] + 60;
+        timers.otherCar = 0;
+    }
 }
 
 /**
@@ -106,7 +133,7 @@ function fixRoads() {
     this.roads.forEach((road, i) => {
         contains[i] = this.camera.worldView.contains(road.x, road.y);
     });
-    console.log( Object.values(contains).toString() );
+    // console.log( Object.values(contains).toString() );
     const secondRoad = this.roads[1];
     if (!this.camera.worldView.contains(secondRoad.x, secondRoad.y)) {
         const firstRoad = this.roads.shift();
@@ -116,7 +143,6 @@ function fixRoads() {
 }
 
 function addLog(text) {
-    const today = new Date();
     const basePos = this.logs.length > 0 ? this.logs[this.logs.length - 1].y : 0;
     this.logs.push(this.add.text(1030,  basePos + 20, text, { fontSize: '16px', fill: '#FFFFFF', fontFamily: 'Arial' }));
 }
